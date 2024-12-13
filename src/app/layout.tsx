81e59e92f4d986e0fbaee7e3d1c9f76615b7e5c9
Danyl Bielytskyi <paintein666@gmail.com>
2024-12-14 00:00:09 +0200

    feat: add Header and Footer in main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,13 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type {Metadata} from "next";
+import "./globals.scss";
+import {Roboto} from 'next/font/google';
+import {Header} from "@/app/components/Header/Header";
+import Footer from "@/app/components/Footer/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700'], // Укажите нужные веса
 });
 
 export const metadata: Metadata = {
@@ -24,8 +22,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <head>
+      <title>Game store</title>
+    </head>
+      <body className={`${roboto.className}`}>
+        <Header/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
