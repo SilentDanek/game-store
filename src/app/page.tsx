248370d8f4be6b4fd92646d5f7f8ced06ecee246
Danyl Bielytskyi <paintein666@gmail.com>
2024-12-14 19:50:17 +0200

    style: create responsive grid layout for game cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,24 +6,35 @@ export default function Home() {
     const cardTitle = 'The Witcher 3: Wild Hunt Game of the Year Edition'
     return (
 
-        <Box sx={{ padding: 4 }}>
-            <Typography variant="h4" sx={{ mb: 3 }}>
+        <Box sx={{padding: 4}}>
+            <Typography variant="h4" sx={{mb: 3}}>
                 Games Collection
             </Typography>
-            <Grid2  container spacing={4}>
-                <GameCard cardMedia={'/Witcher_3.jpg'}
-                          cardDescription={cardDescription}
-                          cardTitle={cardTitle}
-                          price={500}
-                          rating={5}
-                />
-                <GameCard cardMedia={'/Witcher_3.jpg'}
-                          cardDescription={cardDescription}
-                          cardTitle={cardTitle}
-                          price={500}
-                          rating={5}
-                          discount={{discountedPrice:400, percentage:20}}
-                />
+            <Grid2
+                container
+                spacing={3} // Отступы между элементами
+                justifyContent="center" // Центровка контента
+                alignItems="stretch" // Все карточки растягиваются по высоте
+            >
+                {
+                    [0,1,2,3,4,5].map((i) => (
+                        <Grid2
+                            key={i}
+                            size={{
+                                xs: 12,
+                                sm: 6,
+                                md: 4,
+                            }}
+                        >
+                            <GameCard cardMedia={'/Witcher_3.jpg'}
+                                      cardDescription={cardDescription}
+                                      cardTitle={cardTitle}
+                                      price={500}
+                                      rating={5}
+                            />
+                        </Grid2>
+                    ))
+                }
             </Grid2>
         </Box>
     );
