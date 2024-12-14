efb8e7e4d6011baa049157207f08211cf1ad96e4
Danyl Bielytskyi <paintein666@gmail.com>
2024-12-14 17:08:28 +0200

    feat: initial layout and design of the store page

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,17 +1,15 @@
-import {AppBar, IconButton, Toolbar, Typography} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+'use server'
+import {AppBar, Toolbar, Typography} from "@mui/material";
 
 
-export const Header = () => {
+function Header()  {
     return <AppBar position="sticky" color="primary" component="header">
         <Toolbar>
-            <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                <MenuIcon />
-            </IconButton>
-
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                 Game store
             </Typography>
         </Toolbar>
     </AppBar>
 }
+
+export default Header;
